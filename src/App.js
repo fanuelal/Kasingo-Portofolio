456ccad48ddebf29d71991eb9a4e091109b910f3
fanuealal <fanuelalmawstop@gmial.com>
2022-06-20 21:35:24 +0300

    react file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Kasingo
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Kasingo
         </a>
       </header>
     </div>
